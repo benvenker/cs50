cf05f0fc14727623ca2c7a07bf6f1a3fd88da526
Ben Venker <bvemails@gmail.com>
2014-02-02 14:46:09 -0600

    Take the keyword's characters and put them in an array

---
+++ pset2/vigenere/vigenere-keword.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main (int argc, string argv[])
+{
+    /*   Get the input strings   */
+    if (argc !=2)
+    {
+        printf("Usage: ./vigenere kword\n");
+        return 1;
+    }
+    
+    /* Get the sentence from user 
+    printf("enter a phrase to encrypt: \n");
+    string sentence;
+    sentence = GetString();
+    */
+    
+    string keyword = argv[1];
+    int N = strlen(keyword);
+    int k[N];
+   
+    printf("The keyword is: %s\n", keyword);
+    printf("The third character is: %c\n", keyword[2]);
+    
+    // get the characters of keyword into an array
+    for (int i = 0; i < N; i++)
+    {
+        if (keyword[i] >= 'a' && keyword[i] <= 'z')  // if the character is lower case
+        {
+            keyword[i] = keyword[i] - 32;
+        }
+        
+        k[i] = keyword[i];  // get each character into the array
+        
+        
+        printf("Element %i of k = %c\n", i, keyword[i]);
+    }
+    
+}
