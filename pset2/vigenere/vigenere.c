6d440b7a3cdf7e04a45ccc59baba39197ff03c61
Ben Venker <bvemails@gmail.com>
2014-02-03 19:24:10 -0600

    fixed bugs, passed check50

--- pset2/vigenere/vigenere.c
+++ pset2/vigenere/vigenere.c
@@ -7,49 +7,61 @@
 int main (int argc, string argv[])
 {
     /*   Get the input strings   */
-    if (argc !=2)
+    if ( (argc != 2) )
     {
-        printf("Usage: ./vigenere kword\n");
+        printf("Usage: ./vigenere <keyword>\n");
         return 1;
     }
+   
+    for (int i = 0; i < strlen(argv[1]); i++)
+    {
+        if (!(isalpha(argv[1][i])))
+        { 
+        printf("Keyword must only contain letters A-Z and a-z\n");
+        return 1; 
+        }
+    }
+  
+    
+    /*  Create an array and populate it with the keyword's characters */    
+    string keyword = argv[1];
+    int j = strlen(keyword);
+    int k[j];
+    int count = 0;
+    
+    for (int i = 0; i < j; i++)
+    {   
+            if (keyword[i] >= 'a' && keyword[i] <= 'z')  // if the character is lower case
+            {
+                keyword[i] = keyword[i] - 32;
+            }
+            
+            k[i] = keyword[i] -65;  // get each character into the array
+    }
+/* **************************************************************** */  
     
     /* Get the sentence from user */
     string sentence;
     sentence = GetString();
-  
-/*  Create an array and populate it with the keyword's characters */    
-    
-    string keyword = argv[1];
-    int j = strlen(keyword);
-    int k[j];
-    
-    for (int i = 0; i < j; i++)
-    {
-        if (keyword[i] >= 'a' && keyword[i] <= 'z')  // if the character is lower case
-        {
-            keyword[i] = keyword[i] - 32;
-        }
-        
-        k[i] = keyword[i] -65;  // get each character into the array
-        
-        // printf("Element %i of k = %c\n", i, k[i]);
-    }
-/* **************************************************************** */   
-   
     
     // encipher
     for (int j=0, n = strlen(sentence); j < n; j++)
     {
+        if (isalpha(sentence[j])){
+        
         if((sentence[j] >= 'A') && (sentence[j] <= 'Z'))
-            // cipher formula
-            sentence[j] = (((sentence[j] - 'A') + k[j % strlen(keyword)]) % 26) + 'A';;
-            
+        {
+            sentence[j] = (((sentence[j] - 'A') + k[count % strlen(keyword)]) % 26) + 'A';
+        }
         if((sentence[j] >= 'a') && (sentence[j] <='z'))
-            sentence[j] = (((sentence[j] - 'a') + k[j % strlen(keyword)]) % 26) + 'a';
-            
+        {
+            sentence[j] = (((sentence[j] - 'a') + k[count % strlen(keyword)]) % 26) + 'a';
+            //printf("sentence[%i] = (((sentence[%i] - 'a') + k[%i mod %i = %i]) mod 26) + 'a';\n",j,j,count, strlen(keyword), count % strlen(keyword));
+        }
+                count ++;  
+        }
         printf("%c", sentence[j]);    
     }
     printf("\n");
-    
 
 }
