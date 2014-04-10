d1997037731344bbda193f8b72fe38ef1709ce20
Ben Venker <bvemails@gmail.com>
2014-04-09 23:42:28 -0500

    defined $positions

--- pset7/public/index.php
+++ pset7/public/index.php
@@ -3,7 +3,26 @@
     // configuration
     require("../includes/config.php"); 
 
-    // render portfolio
-    render("portfolio.php", ["cash" => 10000.00, "title" => "Portfolio"]);
+    // define positions
+    $positions = [];
+    foreach ($rows as $row)
+    {
+        $stock = lookup($row["symbol"]);
+        if ($stock !== false)
+        {
+            $positions[] = [
+                "name" => $stock["name"],
+                "price"=> $stock["price"],
+                "shares" => $row["shares"],
+                "symbol" => $row["symbol"]
+            ];
+        }
+    }
+    
+    // render positions
+    render("portfolio.php", ["positions" => $positions, "title" => "Portfolio"]);
 
+    // render current cash holdings
+    render("portfolio.php", ["cash" => 10000.00, "title" => "Portfolio"]);
+ 
 ?>
